f91c02aa122f6f875c374bed6e798b0e93ac0b5c
TimsManter <timsmanter@gmail.com>
2017-07-17 15:37:22 +0200

    Update configuration files

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,33 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js")
 
-module.exports = {
-  entry: "./src/tsx/App.tsx",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        include: path.resolve(__dirname, "src/scss"),
-        use: [
-          { loader: "style-loader" },
-          { loader: "css-loader" },
-          { loader: "sass-loader" }
-        ]
-      },
-      // // Look into docs directory to use Babel
-      // { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" },
-      {
-        enforce: 'pre',
-        test: /\.tsx?$/,
-        loader: 'tslint-loader',
-        exclude: /(node_modules)/,
-        options: {
-          configFile: 'tslint.json'
-        }
-      },
-      {
-        test: /\.tsx?$/,
-        loader: "awesome-typescript-loader"
-      },
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        loader: "source-map-loader"
-      }
-    ]
-  },
-  devtool: "source-map",
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, "dist")
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/ejs/index.ejs"
+module.exports = function (env) {
+  if (env === "prod") {
+    return merge(common, {
+      plugins: [
+        new webpack.DefinePlugin({
+          'process.env': {
+            NODE_ENV: '"production"'
+          }
+        }),
+        new webpack.LoaderOptionsPlugin({
+          minimize: true,
+          debug: false
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+          warnings: false,
+          beautify: false,
+          mangle: {
+            screw_ie8: true,
+            keep_fnames: true
+          },
+          compress: {
+            screw_ie8: true
+          },
+          comments: false
+        }),
+      ]
     })
-  ]
-};
+  } else { return common; }
+}
